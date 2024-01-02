a236f6ed0367b1fb82e5a6b40d2f079804fc6371
Joseph dat Dev <139043824+josephDeb@users.noreply.github.com>
2024-01-02 05:58:00 -0300

    setup

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+ //utils
+ import connectDB from './config/db.js' 
+
+dotenv.config();
+
+const port = process.env.PORT || 8088;
+
+connectDB()
+
+ 
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.static("public"))
+ app.use(cors(
+  {
+  origin: ["http://localhost:5173/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+  }
+));
+
+app.get("/", (req, res) => {
+    res.send("BACKEND RUNNING")
+})
+
+app.listen(port, () => {
+    console.log("SERVER RUNNING ON PORT")
+})
+
