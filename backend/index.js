fe642fa728928bc42435cd8c123524974ec9e5a3
Joseph dat Dev <139043824+josephDeb@users.noreply.github.com>
2024-01-02 06:17:05 -0300

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(express.static("public"))
- app.use(cors(
-  {
-  origin: ["http://localhost:5173/"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-  }
+app.use(cors(
+    {
+    origin: ["http://localhost:5173/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+    }
 ));
 
 app.get("/", (req, res) => {
